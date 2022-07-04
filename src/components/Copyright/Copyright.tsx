616a1b97e20d2859b80e016448f61c5f0fcefc79
Ronan Pardo Soares <ronan.soares@gmail.com>
2022-07-04 09:41:21 -0300

    Update Copyright to use bootstrap

--- src/components/Copyright/Copyright.tsx
+++ src/components/Copyright/Copyright.tsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 
 export default function Copyright() {
   const { t } = useTranslation();
   return (
-    <div>
+    <Container>
       {t("Copyright")}{" "}
       <a target="_blank" rel="noreferrer" href="https://seuronao.github.io">
         Ronan Soares
       </a>
-    </div>
+    </Container>
   );
 }
