ae449ab33ebad2350d7dc50193fdc14e78d0540a
Ronan Pardo Soares <ronan.soares@gmail.com>
2022-07-04 10:20:05 -0300

    Add login button

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,17 +1,35 @@
 import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
+import { useUserAuth } from "../../contexts/Auth/UserAuthContext";
 
 export default function Navigation() {
   const { t } = useTranslation();
   return (
     <Navbar bg="light" variant="light">
-      <Container className="justify-content-start">
+      <Container>
         <Navbar.Brand href="/">{t("Navigation.brand")}</Navbar.Brand>
-        <Nav.Link href="/">{t("Navigation.links.home")}</Nav.Link>
-        <Nav.Link href="/schedule">{t("Navigation.links.schedule")}</Nav.Link>
-        <Nav.Link href="/clients">{t("Navigation.links.clients")}</Nav.Link>
+        <Nav className="me-auto">
+          <Nav.Link href="/">{t("Navigation.links.home")}</Nav.Link>
+          <Nav.Link href="/schedule">{t("Navigation.links.schedule")}</Nav.Link>
+          <Nav.Link href="/clients">{t("Navigation.links.clients")}</Nav.Link>
+        </Nav>
+        <Profile />
       </Container>
     </Navbar>
   );
+}
+
+function Profile() {
+  const { t } = useTranslation();
+  const user = useUserAuth().user;
+  if (user) {
+    return <div>{user.displayName}</div>;
+  } else {
+    return (
+      <Button variant="outline-success" href="/signIn">
+        {t("Profile.use")}
+      </Button>
+    );
+  }
 }
