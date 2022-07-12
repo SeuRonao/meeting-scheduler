48dcc7d0572e320e6ea8b55dc021d8d7b8b4b49d
Ronan Pardo Soares <ronan.soares@gmail.com>
2022-07-12 11:10:14 -0300

    Add form to create client

--- src/pages/Clients.tsx
+++ src/pages/Clients.tsx
@@ -1,3 +1,10 @@
+import AddClient from "../components/AddClient/AddClient";
+
 export default function Clients() {
-  return <h1>Clients</h1>;
+  return (
+    <>
+      <h1 className="text-center my-3">Clients</h1>
+      <AddClient />
+    </>
+  );
 }
