ea16eff2d32d6d2cbb5c35f28ee43ed58b1c6053
Ronan Pardo Soares <ronan.soares@gmail.com>
2022-07-19 15:54:55 -0300

    Update Copyright to stay in middle of line

--- src/components/Copyright/Copyright.test.tsx
+++ src/components/Copyright/Copyright.test.tsx
@@ -1,14 +1,25 @@
 import { render, screen } from "@testing-library/react";
 import Copyright from "./Copyright";
 
-test("renders name of site author", () => {
-  render(<Copyright />);
-  const component = screen.getByRole("link");
-  expect(component).toHaveTextContent("Ronan Soares");
-});
+describe("Testing Copyright", () => {
+  test("renders name of site author", () => {
+    render(<Copyright />);
+    const componentList = screen.getAllByRole("link");
+    const linkToProfile = componentList.filter((component) => {
+      return component.textContent?.includes("Ronan Soares");
+    });
+    expect(linkToProfile.length).toEqual(1);
+  });
 
-test("renders link to site author", () => {
-  render(<Copyright />);
-  const component = screen.getByRole("link");
-  expect(component).toHaveAttribute("href", "https://seuronao.github.io");
+  test("renders link to site author", () => {
+    render(<Copyright />);
+    const componentList = screen.getAllByRole("link");
+    const linkToProfile = componentList.filter((component) => {
+      return component.textContent?.includes("Ronan");
+    });
+    expect(linkToProfile.pop()).toHaveAttribute(
+      "href",
+      "https://seuronao.github.io"
+    );
+  });
 });
--- src/components/Copyright/Copyright.tsx
+++ src/components/Copyright/Copyright.tsx
@@ -1,15 +1,24 @@
 import { Container } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
+import { FaGithub } from "react-icons/fa";
 
 export default function Copyright() {
   const { t } = useTranslation("translation", {
     keyPrefix: "components.Copyright",
   });
   return (
-    <Container>
+    <Container className="d-flex justify-content-center align-itens-center">
       {t("made-with-love")}{" "}
       <a target="_blank" rel="noreferrer" href="https://seuronao.github.io">
         Ronan Soares
+      </a>{" "}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://github.com/SeuRonao/meeting-scheduler"
+      >
+        <FaGithub />
+        <span className="visually-hidden">Source Code</span>
       </a>
     </Container>
   );
