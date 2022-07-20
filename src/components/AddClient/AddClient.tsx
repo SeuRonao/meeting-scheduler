590a5da2c55fa0e325d01e79b9fbcf4f231da1c2
Ronan Pardo Soares <ronan.soares@gmail.com>
2022-07-20 09:34:39 -0300

    Improve add client documentation

--- src/components/AddClient/AddClient.tsx
+++ src/components/AddClient/AddClient.tsx
@@ -3,6 +3,7 @@ import { Button } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import AddClientModal from "./AddClientModal";
 
+/** Creates a Button to open a Modal form to create a new client */
 export default function AddClient() {
   const { t } = useTranslation("translation", {
     keyPrefix: "components.AddClient",
