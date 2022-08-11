3d4d406feaeca939086098dcdd48ea70bb4327d6
Ronan Pardo Soares <ronan.soares@gmail.com>
2022-08-11 14:07:16 -0300

    Improve visual of copyright footer

--- src/components/Copyright/Copyright.tsx
+++ src/components/Copyright/Copyright.tsx
@@ -8,19 +8,22 @@ export default function Copyright() {
     keyPrefix: "components.Copyright",
   });
   return (
-    <Container className="d-flex justify-content-center align-itens-center">
-      {t("made-with-love")}{" "}
-      <a target="_blank" rel="noreferrer" href="https://seuronao.github.io">
-        Ronan Soares
-      </a>{" "}
-      <a
-        target="_blank"
-        rel="noreferrer"
-        href="https://github.com/SeuRonao/meeting-scheduler"
-      >
-        <FaGithub />
-        <span className="visually-hidden">Source Code</span>
-      </a>
+    <Container className="my-3 d-flex flex-wrap justify-content-around align-itens-center">
+      <div>
+        {t("made-with-love")}{" "}
+        <a target="_blank" rel="noreferrer" href="https://seuronao.github.io">
+          Ronan Soares
+        </a>
+      </div>
+      <div>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/SeuRonao/meeting-scheduler"
+        >
+          <FaGithub /> <span>Source Code</span>
+        </a>
+      </div>
     </Container>
   );
 }
